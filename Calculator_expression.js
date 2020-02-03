45d95b3eb35f46b5b797efbe911c8d1db0d6db2f
Nataliia <example.@gmail.com>
2020-02-03 17:47:50 +0200

    updated version with swtich

--- Calculator_expression.js
+++ Calculator_expression.js
@@ -1,45 +1,59 @@
 var expressionInput = document.getElementById("Expression");
 var resultLabel = document.getElementById("result");
-expressionInput.addEventListener("keyup", updateResults2);
-result.addEventListener("keyup", updateResults2);
+expressionInput.addEventListener("keyup", updateResults);
+result.addEventListener("keyup", updateResults);
 
-function updateResults2() {
-   var expression = expressionInput.value;
-   var operatorIndex = -1;
-   var indexOfPlus = expression.indexOf('+')
-   var indexOfMinus = expression.indexOf('-')
-   var indexOfMultiply = expression.indexOf('*')
-   var indexOfDivide = expression.indexOf('/')
-   var result;
+function updateResults() {
+  var expression = expressionInput.value;
+  var operatorIndex = -1;
+  var indexOfPlus = expression.indexOf('+');
+  var indexOfMinus = expression.indexOf('-');
+  var indexOfMultiply = expression.indexOf('*');
+  var indexOfDivide = expression.indexOf('/');
+  var result;
+  var operator;
 
+  // for each operator
+  if (indexOfPlus != -1) {
+    operatorIndex = indexOfPlus;
+    operator = 1;
 
-   
+  } else if (indexOfMinus != -1) {
+    operatorIndex = indexOfMinus;
+    operator = 2;
 
-   // for each operator
-   if (indexOfPlus != -1) {
-      operatorIndex = indexOfPlus;
-      var a = parseFloat(expression.substring(0, operatorIndex));
-      var b = parseFloat(expression.substring(operatorIndex + 1));
+  } else if (indexOfMultiply != -1) {
+    operatorIndex = indexOfMultiply;
+    operator = 3;
+
+  } else if (indexOfDivide != -1) {
+    operatorIndex = indexOfDivide;
+    operator = 4;
+
+  }
+  var a = parseFloat(expression.substring(0, operatorIndex));
+  var b = parseFloat(expression.substring(operatorIndex + 1));
+  switch (operator) {
+    case 1: {
       result = a + b;
-
-   } else if (indexOfMinus != -1) {
-      operatorIndex = indexOfMinus;
-      var a = parseFloat(expression.substring(0, operatorIndex));
-      var b = parseFloat(expression.substring(operatorIndex + 1));
+      break;
+    }
+    case 2: {
       result = a - b;
-
-   } else if (indexOfMultiply != -1) {
-      operatorIndex = indexOfMultiply;
-      var a = parseFloat(expression.substring(0, operatorIndex));
-      var b = parseFloat(expression.substring(operatorIndex + 1));
+      break;
+    }
+    case 3: {
       result = a * b;
-
-   } else if (indexOfDivide != -1) {
-      operatorIndex = indexOfDivide;
-      var a = parseFloat(expression.substring(0, operatorIndex));
-      var b = parseFloat(expression.substring(operatorIndex + 1));
+      break;
+    }
+    case 4: {
       result = a / b;
-   }
-  
-   resultLabel.innerHTML = result;
+      break;
+    }
+  }
+
+  resultLabel.innerHTML = result;
 }
+
+//a and b should be outside if
+// then use switch to check operators 
